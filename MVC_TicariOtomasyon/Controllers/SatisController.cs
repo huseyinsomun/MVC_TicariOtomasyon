d3373f6cfafe91d44176bd7ad5c864750a2291d1
Hüseyin <66850222+huseyinsomun@users.noreply.github.com>
2021-01-20 02:52:36 +0300

    Add project files.

---
+++ MVC_TicariOtomasyon/Controllers/SatisController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_TicariOtomasyon.Models.Siniflar;
+
+namespace MVC_TicariOtomasyon.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satıs
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.SatisHarekets.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ MVC_TicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_TicariOtomasyon.Models.Siniflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int CariID { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30,ErrorMessage ="En falza 30 karakter girebilirsiniz.")]
+        [Required(ErrorMessage = "Bu alanı boş geçemezsiniz!")]
+        public string CariAd { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        [Required(ErrorMessage ="Bu alanı boş geçemezsiniz!")]
+        public string CariSoyad { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(13)]
+        [Required(ErrorMessage = "Bu alanı boş geçemezsiniz!")]
+        public string CariSehir { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
+        public string CariEmail { get; set; }
+        public bool Durum { get; set; }
+
+        public ICollection<SatisHareket> SatisHarekets { get; set; }
+
+    }
+}
